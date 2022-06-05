9ee6da4deb6081fd4f56a6d96a997c84a54d5fec
DLBC Baltimore PC <deeperlifebaltimore1@gmail.com>
2022-06-05 08:28:58 -0400

    initial

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+<?php include './utils/events.php' ?>
+
+<html>
+  <head>
+    <title><?php echo "DLBC Baltimore | Home" ?></title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" 
+
+
+content="width=device-width,initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="./assets/styles/nav.css" rel='stylesheet'>
+    <link href="./assets/styles/footer.css" rel='stylesheet'>
+    
+    <link href='./assets/styles/main.css' rel='stylesheet'>
+    <link href='./assets/styles/home.css' rel='stylesheet'>
+    
+</head>
+
+  <body>
+
+    <?php include './assets/templates/navbar.html' ?>
+
+    <div class='web-grid'>
+
+      <main> 
+      
+        <?php 
+          $schedule = getWeekSchedule(); 
+          foreach($schedule as $e){
+            echo $e['title'] . date(' M d ',$e['time']) . ' | ';
+          }
+        ?>
+
+        <section class="weekly">
+
+          <?php 
+
+            foreach($schedule as $event)
+
+              $eventStatus = "Upcoming";
+
+              if(date('d',$event['time']) === date('d')){
+                $eventStatus = "Happening Today";
+              }
+
+              echo('<div class="shadow">' . $eventStatus .
+              '<h3 class="event_date">' . date(' M d ',$event['time']) . '</h2>
+              <p class="event_desc lead">' . $event['title'] . '</p>
+              </div>');
+
+          ?>
+
+          <div class='shadow' >
+            <h3 class="event_date"><?php echo date("M d") ?></h2>
+            <p class="event_desc lead">Sunday Service</p>
+          </div>
+        </section>
+
+      </main>
+
+      <footer>
+        <?php include './assets/templates/footer.html' ?>
+      </footer>
+      
+    </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script>
+      console.log('hello');
+    </script>
+  </body>
+
+</html>
---
+++ utils/events.php
@@ -0,0 +1,46 @@
+<?php
+
+    $events = array(
+        array("title"=>"Sunday Worship Service","date"=>date("w"))
+    );
+
+    function getWeekSchedule(){
+
+        $sundayService;
+        $tuesdayBibleStudy;
+
+        $timeCheck = time();        
+             
+        for($t = 0; $t < 7; $t++){
+
+            $checkDate = getdate($timeCheck);
+
+            switch($checkDate['wday']) {
+
+                case 0:
+
+                    $sundayService = $timeCheck;
+
+                    break;
+                case 2:
+                   $tuesdayBibleStudy = $timeCheck;
+                case 4:
+                    $revivalHour = $timeCheck;
+                
+
+
+            }
+
+            $timeCheck += 86400;
+
+        }
+
+        return array(
+            array('title'=>'Sunday Worship Service','time'=>$sundayService),
+            array('title'=>'Tuesday Bible Study','time'=>$tuesdayBibleStudy),
+            array('title'=>'Thursday Revival Hour','time'=>$revivalHour)
+        );
+
+    }
+
+?>
